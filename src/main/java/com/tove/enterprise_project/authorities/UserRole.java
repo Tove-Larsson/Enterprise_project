3b7a78777daef33293818fd29093ed4765a1a4e5
Tove-Larsson <144339361+Tove-Larsson@users.noreply.github.com>
2024-11-11 17:53:10 +0100

    Create UserRole enum

---
+++ src/main/java/com/tove/enterprise_project/authorities/UserRole.java
@@ -0,0 +1,38 @@
+package com.tove.enterprise_project.authorities;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.tove.enterprise_project.authorities.UserPermission.*;
+
+public enum UserRole {
+
+    ADMIN(GET,POST,PUT,DELETE),
+    USER(GET,POST);
+
+    private final List<String> permissions;
+
+    UserRole(UserPermission... permissionList) {
+        this.permissions = Arrays.stream(permissionList)
+                .map(UserPermission::getPermission)
+                .toList();
+    }
+
+    public List<String> getPermissions() {
+        return permissions;
+    }
+
+    public List<SimpleGrantedAuthority> getAuthorities() {
+
+        List<SimpleGrantedAuthority> simpleGrantedAuthorityList = new ArrayList<>();
+
+        simpleGrantedAuthorityList.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        simpleGrantedAuthorityList.addAll(getPermissions().stream().map(SimpleGrantedAuthority::new)
+                .toList());
+
+        return simpleGrantedAuthorityList;
+    }
+}
