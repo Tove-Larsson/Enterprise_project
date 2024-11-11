1f2a36ffb59fc86f6c244b16c331de7d4999bc2c
Tove-Larsson <144339361+Tove-Larsson@users.noreply.github.com>
2024-11-11 16:45:16 +0100

    Create DB connection, add AppUser entity

---
+++ src/main/java/com/tove/enterprise_project/model/AppUser.java
@@ -0,0 +1,45 @@
+package com.tove.enterprise_project.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    public AppUser() {
+    }
+
+    public AppUser(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- src/main/java/com/tove/enterprise_project/repository/UserRepository.java
+++ src/main/java/com/tove/enterprise_project/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package com.tove.enterprise_project.repository;
 
-import com.tove.enterprise_project.model.User;
+import com.tove.enterprise_project.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<AppUser, Long> {
 
 }
